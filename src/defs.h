7839ccd2c6deae75ea4332b895fae37b643b8bf4
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2022-05-09 13:32:47 +0200

    Update source code defines

--- src/defs.h
+++ src/defs.h
@@ -4,10 +4,13 @@
 /* compatibility with X/Open 7 (POSIX 2008) */
 #define _XOPEN_SOURCE 700
 
+/* compatibility with POSIX 2008 */
+#define _POSIX_C_SOURCE 200809L
+
 /* include most default macros */
 #define _GNU_SOURCE
-//#define _BSD_SOURCE
-//#define _DEFAULT_SOURCE
+
+
 
 #define UNUSED __attribute__ ((unused))
 
