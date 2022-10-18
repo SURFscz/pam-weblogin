ff9d659293165d4a7c4a92eee74bf29dd0861819
Martin van Es <martin@mrvanes.com>
2022-10-18 14:48:23 +0200

    Initial working version

--- shell/weblogin.py
+++ shell/weblogin.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python
 import subprocess
+import requests
+import json
+import getpass
 
-# Debug info
-# This is printed before activating the shell
-print("before bash")
+authheader = {
+    'Authorization': 'Bearer Token'
+}
+
+start = {
+    'user_id': 'myshell',
+    'attribute': 'mail',
+    'cache_duration': 60
+}
+
+response = requests.post('http://localhost:8080/pam-weblogin/start', data=json.dumps(start), headers=authheader)
+answer = response.json()
+print(answer['challenge'])
+
+session_id = answer['session_id']
+check = {'session_id': session_id}
+
+counter = 0
+while True:
+    counter += 1
+    if counter > 3:
+        break
+    code = getpass.getpass("code: ")
+    check['pin'] = code
+    response = requests.post('http://localhost:8080/pam-weblogin/check-pin', data=json.dumps(check), headers=authheader)
+    answer = response.json()
+    result = answer['result']
+    if result == 'SUCCESS':
+        break
+
 subprocess.call(['sudo', '-iu', 'myshell'])
-
-# This is printed after exiting the shell
-print("after bash")
