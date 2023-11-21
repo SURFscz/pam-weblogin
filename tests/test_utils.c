206cd5372f42dac671226dfb4c8e343f188aced6
Martin van Es <martin@mrvanes.com>
2023-11-21 09:43:32 +0100

    Fix test

--- tests/test_utils.c
+++ tests/test_utils.c
@@ -125,7 +125,7 @@ const char json_txt[] = "{                      " \
 	"\"key_int\": 1,                            " \
 	"\"key_str\": \"test\",                     " \
 	"\"key_str_empty\": \"\",                   " \
-	"\"key_obj\": { \"key_sub\": \"val_sub\" }  " \
+	"\"key_obj\": { \"key_sub\": \"val_sub\" }, " \
 	"\"key_array\": [ \"one\", \"two\" ]        " \
 "}";
 
@@ -224,11 +224,13 @@ START_TEST(test_json_utils)
 	ck_assert_str_eq(result4, "val_sub");
 
 	/* get first index */
-	char *result5 = getIndex(findKey(json, "key_array"), 0);
+	json_value *item1 = getIndex(findKey(json, "key_array"), 0);
+	char *result5 = item1->u.string.ptr;
 	ck_assert_str_eq(result5, "one");
 
 	/* get second index */
-	char *result6 = getIndex(findKey(json, "key_array"), 1);
+	json_value *item2 = getIndex(findKey(json, "key_array"), 1);
+	char *result6 = item2->u.string.ptr;
 	ck_assert_str_eq(result6, "two");
 
 	/* convenience function to get string or bool */
