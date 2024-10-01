23e214bfdc04e408dbf4ccfb46924be393517fb2
Martin van Es <martin@mrvanes.com>
2024-10-01 20:42:03 +0200

    Add new test

--- tests/test_config.c
+++ tests/test_config.c
@@ -9,17 +9,32 @@
 
 #include "../src/config.h"
 
-START_TEST (test_getConfig)
+START_TEST (test_getConfig_1)
 {
 	/* Read configuration file */
 	Config *cfg = NULL;
-	cfg = getConfig("pam-weblogin.conf");
+	cfg = getConfig("pam-weblogin.conf_1");
 	ck_assert_ptr_ne(cfg, NULL);
 	ck_assert_str_eq(cfg->url, "https://sbs.scz-vm.net/pam-weblogin");
 	ck_assert_str_eq(cfg->token, "Bearer test");
 	ck_assert_int_eq(cfg->retries, 3);
 	ck_assert_str_eq(cfg->attribute, "uid");
 	ck_assert_int_eq(cfg->cache_duration, 60);
+	ck_assert(cfg->pam_user);
+}
+END_TEST
+
+START_TEST (test_getConfig_2)
+{
+	/* Read configuration file */
+	Config *cfg = NULL;
+	cfg = getConfig("pam-weblogin.conf_2");
+	ck_assert_ptr_ne(cfg, NULL);
+	ck_assert_str_eq(cfg->url, "https://sbs.scz-vm.net/pam-weblogin");
+	ck_assert_str_eq(cfg->token, "Bearer test");
+	ck_assert_int_eq(cfg->retries, 3);
+	ck_assert_str_eq(cfg->attribute, "uid");
+	ck_assert_int_eq(cfg->cache_duration, 0);
 	ck_assert(cfg->pam_user);
 }
 END_TEST
@@ -31,7 +46,8 @@ TCase * test_config(void)
 	tcase_set_timeout(tc, 7200);
 
 	/* getConfig() */
-    tcase_add_test(tc, test_getConfig);
+    tcase_add_test(tc, test_getConfig_1);
+    tcase_add_test(tc, test_getConfig_2);
 
 	return tc;
 }
