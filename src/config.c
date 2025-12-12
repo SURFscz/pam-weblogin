d74ea7c9393d181cb5a62ce0e5745c9672d8a1ab
Martin van Es <martin@mrvanes.com>
2025-12-12 13:27:07 +0100

    wip

--- src/config.c
+++ src/config.c
@@ -90,7 +90,7 @@ Config *getConfig(const char *filename)
 	int lineno = 0;
 	FILE *fp = NULL;
 
-	Config *cfg = (Config *) malloc(sizeof(Config));
+	Config *cfg = (Config *)malloc(sizeof(Config));
 	if (cfg == NULL)
 	{
 		log_message(LOG_ERR, "Can't allocate memory");
